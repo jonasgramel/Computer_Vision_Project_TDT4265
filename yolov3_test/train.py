5a94bbf69fcb502093cc9b52fab0810e4684fdc7
Jonas Gran Melandsor <jonasgm@stud.ntnu.no>
2025-04-09 20:53:48 +0200

    Back up

--- yolov3_test/train.py
+++ yolov3_test/train.py
@@ -50,7 +50,7 @@ batch_size = 32
 leanring_rate = 1e-5
 
 # Number of epochs for training 
-epochs = 20
+epochs = 10
 
 # Image size 
 image_size = 416
@@ -209,9 +209,9 @@ test_transform = A.Compose(
 
 # Creating a dataset object 
 dataset = Dataset( 
-	csv_file="train.csv", 
-	image_dir="images/", 
-	label_dir="labels/", 
+	#csv_file="train.csv", 
+	image_dir="/work/datasets/tdt4265/ad/open/Poles/lidar/combined_color/train", 
+	label_dir="/work/datasets/tdt4265/ad/open/Poles/lidar/labels/train", 
 	grid_sizes=[13, 26, 52], 
 	anchors=ANCHORS, 
 	transform=test_transform 
@@ -295,12 +295,9 @@ def training_loop(loader, model, optimizer, loss_fn, scaler, scaled_anchors):
 		mean_loss = sum(losses) / len(losses) 
 		progress_bar.set_postfix(loss=mean_loss)
 
-    return losses
 
-
-
-test_yolov3 = True
-trainyolov3 = False
+test_yolov3 = False
+trainyolov3 = True
 
 if __name__ == "__main__": 
 
@@ -341,9 +338,9 @@ if __name__ == "__main__":
         train_dataset = Dataset( 
             # csv_file="./data/pascal voc/train.csv", 
             # image_dir="./data/pascal voc/images/", 
-            image_dir = "/work/datasets/tdt4265/ad/open/Poles/lidar/combined_color/images", # For Cybele, lidar images
+            image_dir = "/work/datasets/tdt4265/ad/open/Poles/lidar/combined_color/train", # For Cybele, lidar images
             # label_dir="./data/pascal voc/labels/", 
-            label_dir = "/work/datasets/tdt4265/ad/open/Poles/lidar/combined_color/labels", # For Cybele, lidar labels
+            label_dir = "/work/datasets/tdt4265/ad/open/Poles/lidar/labels/train", # For Cybele, lidar labels
             anchors=ANCHORS, 
             transform=train_transform 
         ) 
@@ -371,4 +368,3 @@ if __name__ == "__main__":
             # Saving the model 
             if save_model: 
                 save_checkpoint(model, optimizer, filename=f"checkpoint.pth.tar")
-        
