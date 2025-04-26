2fed749eefa7a1997bde218bfb9b080bce12d578
Audun Sørheim <audunsor@clab16.idi.ntnu.no>
2025-04-26 14:06:32 +0200

    chagnes

--- yolo11/train.py
+++ yolo11/train.py
@@ -1,7 +1,19 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
-results = model.train(data="data.yaml", epochs=100, imgsz=640)
+
+results = model.train(
+    data="/home/audunsor/Documents/Computer_Vision_Project_TDT4265/yolo11/data.yaml",
+    epochs=50,
+    imgsz=640,
+    lr0=0.001,
+    pretrained=True,
+    freeze=10,
+    batch=32,
+    project="yolo11",
+    name="transfer_learning"
+)
+
 model.predict(
     source="/work/datasets/tdt4265/ad/open/Poles/rgb/images/test",
     project="yolo11",
